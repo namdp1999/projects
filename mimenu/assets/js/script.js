643af2d1baf16e832114e9edc3e6519a482e11ea
Nam Dang <61955753+namdp1999@users.noreply.github.com>
2021-10-01 23:25:53 +0700

    Update page

--- mimenu/assets/js/script.js
+++ mimenu/assets/js/script.js
@@ -85,6 +85,13 @@ $(".note-cart-modal .note-close").click(function () {
 });
 // End Note Cart Modal
 
+// Promotion Modal
+$(".checkout-promotion__button").click(function () {
+    $(".modal-promotion").addClass("open");
+    $(".bottom-modal__box").addClass("open");
+});
+// End Promotion Modal
+
 // Get Store Modal
 $(".button-get-store").click(function () {
     $(".modal-get-store").addClass("open");
@@ -102,3 +109,74 @@ $(".button-get-home").click(function () {
     $(".modal-get-store .bottom-modal__box").removeClass("open");
 });
 // End Get Home Modal
+
+// Form Validate
+$(document).ready(function() {
+    $("#form-get-store").validate({
+        rules: {
+            fullname: "required",
+            phone: {
+                required: true,
+                minlength: 10,
+                numericOnly:true
+            }
+        },
+        messages: {
+            fullname: "Vui lòng nhập họ tên!",
+            phone: {
+                required: "Vui lòng nhập số điện thoại!",
+                minlength: "Số điện thoại không chính xác!"
+            }
+        },
+        submitHandler: function(form,e) {
+            $(".bottom-modal").removeClass("open");
+            $(".bottom-modal__box").removeClass("open");
+            $(".checkout-shipping .button-get-store").addClass("active");
+            $(".checkout-shipping .button-get-home").removeClass("active");
+            $('.checkout-shipping__contact-name span').html($("#form-get-store input[name=fullname]").val());
+            $('.checkout-shipping__contact-phone span').html($("#form-get-store input[name=phone]").val());
+            $('.checkout-shipping__contact-seller').removeClass("d-none");
+            $('.checkout-shipping__contact-name').removeClass("d-none");
+            $('.checkout-shipping__contact-phone').removeClass("d-none");
+            $('.checkout-shipping__contact-address').addClass("d-none");
+        }
+    });
+
+    $("#form-get-home").validate({
+        rules: {
+            fullname: "required",
+            phone: {
+                required: true,
+                minlength: 10,
+                numericOnly:true
+            },
+            address: "required"
+        },
+        messages: {
+            fullname: "Vui lòng nhập họ tên!",
+            phone: {
+                required: "Vui lòng nhập số điện thoại!",
+                minlength: "Số điện thoại không chính xác!"
+            },
+            address: "Vui lòng nhập địa chỉ của bạn!"
+        },
+        submitHandler: function(form,e) {
+            $(".bottom-modal").removeClass("open");
+            $(".bottom-modal__box").removeClass("open");
+            $(".checkout-shipping .button-get-home").addClass("active");
+            $(".checkout-shipping .button-get-store").removeClass("active");
+            $('.checkout-shipping__contact-name span').html($("#form-get-home input[name=fullname]").val());
+            $('.checkout-shipping__contact-phone span').html($("#form-get-home input[name=phone]").val());
+            $('.checkout-shipping__contact-address span').html($("#form-get-home input[name=address]").val());
+            $('.checkout-shipping__contact-seller').addClass("d-none");
+            $('.checkout-shipping__contact-name').removeClass("d-none");
+            $('.checkout-shipping__contact-phone').removeClass("d-none");
+            $('.checkout-shipping__contact-address').removeClass("d-none");
+        }
+    });
+});
+
+$.validator.addMethod('numericOnly', function (value) {
+    return /^[0-9]+$/.test(value);
+}, 'Số điện thoại không chính xác!');
+// End Form Validate
