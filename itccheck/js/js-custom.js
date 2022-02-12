60c2ff8a8dcc7a3f1f50a964c8ffffaefb371199
Nam Dang <61955753+namdp1999@users.noreply.github.com>
2022-02-13 00:25:53 +0700

    Add project itccheck

---
+++ itccheck/js/js-custom.js
@@ -0,0 +1,75 @@
+// Menu Mobile
+$(".header-menu__btn-open").click(function () {
+  $(".header-menu").addClass("show");
+});
+
+$(".header-menu__btn-close").click(function () {
+  $(".header-menu").removeClass("show");
+});
+
+$(".header-menu__overlay").click(function () {
+  $(".header-menu").removeClass("show");
+});
+
+$(".header-menu__item").click(function () {
+  $(this).toggleClass("show");
+});
+// End Menu Mobile
+
+// Scroll To Top
+$("#return-to-top").click(function() {
+  $(window).scrollTop(0);
+});
+// End Scroll To Top
+
+// Intro Help Slide
+var js_banner_slide = new Swiper(".js-intro-help-slide", {
+  slidesPerView: 2,
+  spaceBetween: 8,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 2,
+    },
+  },
+});
+// End Intro Help Slide
+
+// News Slide
+var js_banner_slide = new Swiper(".js-news-slide", {
+  slidesPerView: 3,
+  spaceBetween: 8,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+// End News Slide
+
+// Why Choose Video
+lightGallery(document.getElementById('js-why-choose-video'));
+// End Why Choose Video
